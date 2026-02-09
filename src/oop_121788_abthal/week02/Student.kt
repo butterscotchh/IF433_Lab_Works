d04e7896671e5a5117b1e950fc328ce0ff73fcac
butterscotchh <sampansedang@gmail.com>
2026-02-09 16:39:01 +0700

    week02: add secondary constructor

--- src/oop_121788_abthal/week02/Student.kt
+++ src/oop_121788_abthal/week02/Student.kt
@@ -7,12 +7,15 @@ class Student(
 ) {
 
     init {
-        // Validasi sederhana: cek panjang NIM
         if (nim.length != 5) {
             println("[WARNING] Objek tercipta dengan NIM ($nim) yang tidak valid")
-            println("Data mahasiswa masih mungkin akan bermasalah di sistem.")
         } else {
             println("[LOG] Objek Student berhasil dialokasikan di memory.")
         }
+    }
+
+    // Secondary Constructor (mirip Java)
+    constructor(name: String, nim: String) : this(name, nim, "Undeclared") {
+        println("[INFO] Jurusan default digunakan: Undeclared")
     }
 }
