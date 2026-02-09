58c9ff5c1d7f898669071ab80f2350f0e16be8a3
butterscotchh <sampansedang@gmail.com>
2026-02-09 11:10:14 +0700

    week01: implement null safety check

--- src/oop_121788_abthal/week01/GradingSystem.kt
+++ src/oop_121788_abthal/week01/GradingSystem.kt
@@ -17,4 +17,9 @@ fun main() {
     println("Grade kamu: $grade")
 
     println("Status: ${calculateStatus(score)}")
+
+    val studentId: String? = null
+    val idLength = studentId?.length ?: 0
+
+    println("Panjang ID: $idLength")
 }
