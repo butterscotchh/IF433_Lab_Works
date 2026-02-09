9266e7471590bab6d577a3976c2d72c29b78fa82
butterscotchh <sampansedang@gmail.com>
2026-02-09 11:25:45 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_121788_abthal/week01/GameStore.kt
+++ src/oop_121788_abthal/week01/GameStore.kt
@@ -1,2 +1,12 @@
 package oop_121788_abthal.week01
 
+fun main() {
+    val gameTitle = "SteamKW Deluxe Edition"
+    val price = 550_000
+
+    val discount = calculateDiscount(price)
+}
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500_000) price * 20 / 100
+    else price * 10 / 100
