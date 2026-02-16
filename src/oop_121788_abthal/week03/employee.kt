b9fffffa7ba642ae3483cf2c7e248f650e9c00e0
butterscotchh <sampansedang@gmail.com>
2026-02-16 22:14:06 +0700

    week03: fix setter using backing field

--- src/oop_121788_abthal/week03/employee.kt
+++ src/oop_121788_abthal/week03/employee.kt
@@ -2,4 +2,12 @@ package oop_121788_abthal.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                field = 0   // Gunakan field, bukan this.salary
+            } else {
+                field = value   // Gunakan field untuk assign nilai asli
+            }
+        }
 }
