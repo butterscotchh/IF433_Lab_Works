5353162dcc58ad98db381908cd077acb815b4341
butterscotchh <sampansedang@gmail.com>
2026-02-16 22:27:13 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_121788_abthal/week03/Task1/Main.kt
+++ src/oop_121788_abthal/week03/Task1/Main.kt
@@ -1,2 +1,17 @@
 package oop_121788_abthal.week03.Task1
 
+fun main() {
+
+    val weapon = Weapon("Excalibur")
+
+    // Test 1: Damage negatif (harus gagal)
+    weapon.damage = -50
+    println("Damage sekarang: ${weapon.damage}")
+
+    // Test 2: Damage terlalu besar (harus jadi 1000)
+    weapon.damage = 9999
+    println("Damage sekarang: ${weapon.damage}")
+
+    // Print Tier
+    println("Tier senjata: ${weapon.tier}")
+}
--- src/oop_121788_abthal/week03/Task1/weapon.kt
+++ src/oop_121788_abthal/week03/Task1/weapon.kt
@@ -1,2 +1,25 @@
 package oop_121788_abthal.week03.Task1
 
+class Weapon(val name: String) {
+
+    var damage: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("WARNING: Damage tidak boleh negatif!")
+                // Jangan ubah field (biarkan nilai lama)
+            } else if (value > 1000) {
+                println("WARNING: Damage terlalu besar! Diset ke 1000.")
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    // Computed Property (Custom Getter)
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
