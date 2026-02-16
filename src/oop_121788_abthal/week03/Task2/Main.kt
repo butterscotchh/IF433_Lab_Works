d665485ce1acf5a8442104fcc8d4e15d2c0cbf7b
butterscotchh <sampansedang@gmail.com>
2026-02-16 22:31:20 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_121788_abthal/week03/Task2/Main.kt
+++ src/oop_121788_abthal/week03/Task2/Main.kt
@@ -1,2 +1,17 @@
 package oop_121788_abthal.week03.Task2
 
+fun main() {
+
+    val player = Player("Rikako")
+
+    // Coba akses langsung (harus error jika di-uncomment)
+    // player.xp = 100  // ERROR (karena private)
+
+    println("Level awal: ${player.level}")
+
+    player.addXp(50)   // Masih level 1
+    println("Level sekarang: ${player.level}")
+
+    player.addXp(60)   // Total 110 -> Harus Level Up ke 2
+    println("Level sekarang: ${player.level}")
+}
--- src/oop_121788_abthal/week03/Task2/player.kt
+++ src/oop_121788_abthal/week03/Task2/player.kt
@@ -1,2 +1,27 @@
 package oop_121788_abthal.week03.Task2
 
+class Player(val username: String) {
+
+    // XP tidak boleh diakses langsung dari luar
+    private var xp: Int = 0
+
+    // Computed Property (Level dihitung dari XP)
+    val level: Int
+        get() = (xp / 100) + 1
+
+    fun addXp(amount: Int) {
+
+        if (amount <= 0) {
+            println("XP harus bernilai positif!")
+            return
+        }
+
+        val oldLevel = level
+        xp += amount
+        val newLevel = level
+
+        if (newLevel > oldLevel) {
+            println("Level Up! Selamat $username naik ke level $newLevel")
+        }
+    }
+}
