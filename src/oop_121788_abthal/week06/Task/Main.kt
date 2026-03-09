4dfc4316e8ff1c0895acf49a9e2b2e1f998ed1cf
butterscotchh <sampansedang@gmail.com>
2026-03-10 03:16:12 +0700

    week06: (task) test smart home system logic in main

--- src/oop_121788_abthal/week06/Task/Main.kt
+++ src/oop_121788_abthal/week06/Task/Main.kt
@@ -1,2 +1,20 @@
 package oop_121788_abthal.week06.Task
 
+// Main.kt
+
+fun main() {
+    // Instansiasi Perangkat
+    val lampu = SmartLamp("L01", "Ruang Tamu")
+    val speaker = SmartSpeaker("S01", "Google Nest Dapur")
+    val cctv = SmartCCTV("C01", "Ezviz Garasi")
+
+    // Instansiasi Hub dan Tambah Perangkat
+    val myHub = SmartHomeHub()
+    myHub.addDevice(lampu)
+    myHub.addDevice(speaker)
+    myHub.addDevice(cctv)
+
+    // Eksekusi Logika
+    myHub.activateSecurityMode()
+    myHub.turnOffAllSwitches()
+}
