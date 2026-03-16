73dad79ae347f89e552a9103a1d3eb3d4ca18e12
butterscotchh <sampansedang@gmail.com>
2026-03-16 22:48:26 +0700

    week07: implement data class for structural equality

--- src/oop_121788_abthal/week07/Main.kt
+++ src/oop_121788_abthal/week07/Main.kt
@@ -1,9 +1,9 @@
 package oop_121788_abthal.week07
 
-println("\n=== TEST REGULAR CLASS ===")
+println("\n=== TEST DATA CLASS ===")
 
-val reg1 = RegularUser("Alice", 22)
-val reg2 = RegularUser("Alice", 22)
+val data1 = DataUser("Alice", 22)
+val data2 = DataUser("Alice", 22)
 
-println(reg1)
-println("Sama? ${reg1 == reg2}")
+println(data1)
+println("Sama? ${data1 == data2}")
