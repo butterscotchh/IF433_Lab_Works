570e7efe45e8f831582b4244735e1baccaada50a
butterscotchh <sampansedang@gmail.com>
2026-03-16 23:00:45 +0700

    week07: (trial) trigger non-exhaustive when compiler error

--- src/oop_121788_abthal/week07/Main.kt
+++ src/oop_121788_abthal/week07/Main.kt
@@ -1,37 +1,13 @@
 package oop_121788_abthal.week07
 
-package oop_001_johnthor.week07
+val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
 
-fun main() {
+when(response) {
 
-    println("=== TEST SINGLETON ===")
-    println("Status: ${DatabaseManager.connectionStatus}")
-    DatabaseManager.connect()
+    is ApiResponse.Success ->
+    println("Tampilkan: ${response.data}")
 
-    println("\n=== TEST COMPANION OBJECT ===")
-    val client = NetworkClient.createClient()
-    client.connect()
-
-    println("\n=== TEST REGULAR CLASS ===")
-    val reg1 = RegularUser("Alice", 22)
-    val reg2 = RegularUser("Alice", 22)
-
-    println(reg1)
-    println("Sama? ${reg1 == reg2}")
-
-    println("\n=== TEST DATA CLASS ===")
-    val data1 = DataUser("Alice", 22)
-    val data2 = DataUser("Alice", 22)
-
-    println(data1)
-    println("Sama? ${data1 == data2}")
-
-    println("\n=== TEST COPY ===")
-    val data3 = data1.copy(age = 23)
-    println("Hasil Copy: $data3")
-
-    println("\n=== TEST DESTRUCTURING ===")
-    val (userName, userAge) = data1
-    println("Destructured: $userName berumur $userAge")
+    is ApiResponse.Error ->
+    println("Munculkan alert: ${response.message}")
 
 }
