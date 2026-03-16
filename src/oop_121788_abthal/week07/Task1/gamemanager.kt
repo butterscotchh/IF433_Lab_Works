62c196997d5d5aad1e47e7559f943b8d6eff5051
butterscotchh <sampansedang@gmail.com>
2026-03-16 23:07:49 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_121788_abthal/week07/Task1/gamemanager.kt
@@ -0,0 +1,14 @@
+package oop_121788_abthal.week07.Task1
+
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
