7fa00b2ea15a77fd4c56b9b0d52a3c5bb2abd600
butterscotchh <sampansedang@gmail.com>
2026-03-16 23:27:09 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_121788_abthal/week07/Task1/Main.kt
+++ src/oop_121788_abthal/week07/Task1/Main.kt
@@ -5,7 +5,13 @@ fun main() {
     GameManager.startGame()
 
     println("Drop chance dari ItemRarity.LEGENDARY: ${ItemRarity.LEGENDARY.dropChance}")
-
     val starterWeapon = Weapon.forgeStarterSword()
     println("Detail Senjata: ${starterWeapon.item.name}, Damage: ${starterWeapon.item.damage}, Rarity: ${starterWeapon.item.rarity}")
+
+    val upgradedItem = starterWeapon.item.copy(damage = 25)
+
+    processEvent(SafeZone)
+    processEvent(MonsterEncounter("Goblin Nakal"))
+    processEvent(LootDropped(upgradedItem))
+    processEvent(GameOver("Terkena jebakan racun"))
 }
