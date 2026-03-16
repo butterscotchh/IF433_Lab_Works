50d883aa7f04114cc5660eb78322e2ed764306af
butterscotchh <sampansedang@gmail.com>
2026-03-16 22:54:57 +0700

    week07: implement copy and destructuring on data class

--- src/oop_121788_abthal/week07/Main.kt
+++ src/oop_121788_abthal/week07/Main.kt
@@ -1,9 +1,37 @@
 package oop_121788_abthal.week07
 
-println("\n=== TEST DATA CLASS ===")
+package oop_001_johnthor.week07
 
-val data1 = DataUser("Alice", 22)
-val data2 = DataUser("Alice", 22)
+fun main() {
 
-println(data1)
-println("Sama? ${data1 == data2}")
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient()
+    client.connect()
+
+    println("\n=== TEST REGULAR CLASS ===")
+    val reg1 = RegularUser("Alice", 22)
+    val reg2 = RegularUser("Alice", 22)
+
+    println(reg1)
+    println("Sama? ${reg1 == reg2}")
+
+    println("\n=== TEST DATA CLASS ===")
+    val data1 = DataUser("Alice", 22)
+    val data2 = DataUser("Alice", 22)
+
+    println(data1)
+    println("Sama? ${data1 == data2}")
+
+    println("\n=== TEST COPY ===")
+    val data3 = data1.copy(age = 23)
+    println("Hasil Copy: $data3")
+
+    println("\n=== TEST DESTRUCTURING ===")
+    val (userName, userAge) = data1
+    println("Destructured: $userName berumur $userAge")
+
+}
