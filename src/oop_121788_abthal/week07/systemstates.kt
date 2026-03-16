b4523046fcc3755be88144e36b6a14afce5d5d37
butterscotchh <sampansedang@gmail.com>
2026-03-16 22:57:18 +0700

    week07: create ApiResponse sealed class

--- src/oop_121788_abthal/week07/systemstates.kt
+++ src/oop_121788_abthal/week07/systemstates.kt
@@ -1,2 +1,11 @@
 package oop_121788_abthal.week07
 
+sealed class ApiResponse {
+
+    data class Success(val data: String) : ApiResponse()
+
+    data class Error(val message: String) : ApiResponse()
+
+    object Loading : ApiResponse()
+
+}
