8237d821910f9e2c985812ee9a6c09b20f21f0e7
butterscotchh <sampansedang@gmail.com>
2026-04-13 21:56:37 +0700

    week08: implement safe call with let block

--- src/oop_121788_abthal/week08/Main.kt
+++ src/oop_121788_abthal/week08/Main.kt
@@ -1,2 +1,20 @@
 package oop_121788_abthal.week08
 
+fun main() {
+    println("=== TEST SAFE CALLS & ELVIS ===")
+    val emptyOrder = Order(null, null)
+
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
+    println("Tujuan pengiriman: $destination")
+
+    println("\n=== TEST LET BLOCK ===")
+    val validOrder = Order(null, 250000)
+
+    val receipt = validOrder.totalPrice?.let { price ->
+        // Blok ini HANYA jalan jika totalPrice tidak null
+        val tax = price * 0.11
+        "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
+    } ?: "Transaksi Invalid: Harga belum di-set!"
+
+    println(receipt)
+}
--- src/oop_121788_abthal/week08/NotificationService.kt
+++ src/oop_121788_abthal/week08/NotificationService.kt
@@ -1,2 +1,18 @@
 package oop_121788_abthal.week08
 
+class NotificationService {
+    fun sendEmail(emailAddress: String) {
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser(user: UserProfile) {
+
+
+        if (user.email != null) {
+
+            sendEmail(user.email)
+        } else {
+            println("User ${user.name} tidak memiliki email.")
+        }
+    }
+}
