bf23125be1a28af6af8e9e2fb62224aee10a354e
butterscotchh <sampansedang@gmail.com>
2026-04-13 23:22:30 +0700

    week08: (task) implement payment processing with !!

--- src/oop_121788_abthal/week08/Task1/JavaPaymentService.java
+++ src/oop_121788_abthal/week08/Task1/JavaPaymentService.java
@@ -1,1 +1,7 @@
 package oop_121788_abthal.week08.Task1;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
