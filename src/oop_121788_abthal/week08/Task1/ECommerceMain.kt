da83db7fd79405127275be56309cd72c55b02239
butterscotchh <sampansedang@gmail.com>
2026-04-13 23:28:13 +0700

    week08: (task) test full e-commerce pipeline in main

--- src/oop_121788_abthal/week08/Task1/ECommerceMain.kt
+++ src/oop_121788_abthal/week08/Task1/ECommerceMain.kt
@@ -1,2 +1,24 @@
 package oop_121788_abthal.week08.Task1
 
+fun main() {
+    val rawApiData: List<Map<String, Any?>> = listOf(
+        mapOf("id" to "E01", "name" to "Laptop", "type" to "ELECTRONIC", "warranty" to 24),
+        mapOf("id" to "C01", "name" to "T-Shirt", "type" to "CLOTHING", "size" to "XL"),
+        mapOf("id" to "E02", "name" to "Mouse", "type" to "ELECTRONIC", "warranty" to "Not An Integer"), // Corrupted warranty
+        mapOf("name" to "Ghost Item", "type" to "CLOTHING"), // Missing ID
+        mapOf("id" to "X01", "name" to "Unknown", "type" to "FOOD") // Unknown type
+    )
+
+    val parser = ApiParser()
+
+    for (raw in rawApiData) {
+        try {
+            val product = parser.parseProduct(raw)
+            product?.let {
+                parser.checkout(it)
+            }
+        } catch (e: IllegalArgumentException) {
+            println("Exception ditangkap: ${e.message}")
+        }
+    }
+}
