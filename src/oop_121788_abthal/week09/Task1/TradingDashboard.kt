c9558933377bf45190f0f5be454511ec4348aa20
butterscotchh <sampansedang@gmail.com>
2026-04-20 23:16:32 +0700

    week09: (task) filter CLOSED trades only

--- src/oop_121788_abthal/week09/Task1/TradingDashboard.kt
+++ src/oop_121788_abthal/week09/Task1/TradingDashboard.kt
@@ -1,2 +1,15 @@
 package oop_121788_abthal.week09.Task1
 
+fun main() {
+    // Inisialisasi Data Uji (Minimal 6 TradeLog)
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 5, 25.0, "OPEN"),
+        TradeLog("BTCUSDT", "SHORT", 50, 42.3, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 10, -12.5, "CLOSED"),
+        TradeLog("BNBUSDT", "LONG", 20, 8.0, "CLOSED")
+    )
+
+    val closedTrades = tradeHistory.filter { it.status == "CLOSED" }
+}
--- src/oop_121788_abthal/week09/Task1/TradingModels.kt
+++ src/oop_121788_abthal/week09/Task1/TradingModels.kt
@@ -1,2 +1,9 @@
 package oop_121788_abthal.week09.Task1
 
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
