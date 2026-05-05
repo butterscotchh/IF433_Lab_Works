b873754d43b5f41a854ab3cfc47b685e822fd20e
butterscotchh <sampansedang@gmail.com>
2026-05-05 08:20:26 +0700

    week11: (task) implement device search with let

--- src/oop_121788_abthal/week11/Task1/SmartHomeDashboard.kt
+++ src/oop_121788_abthal/week11/Task1/SmartHomeDashboard.kt
@@ -30,4 +30,10 @@ fun main() {
 
     // Menambahkan alat pakan peliharaan secara langsung
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+
+    val searchResult = homeDevices.find { it.category == "Camera" }
+    searchResult?.let {
+        println("\n--- Hasil Pencarian ---")
+        println(it.diagnose()) // Memanggil extension function diagnose()
+    }
 }
