3cc1748aadd3d5184959df7e99687747ed194474
butterscotchh <sampansedang@gmail.com>
2026-05-05 07:33:42 +0700

    week11: implement run function for object computation

--- src/oop_121788_abthal/week11/Main.kt
+++ src/oop_121788_abthal/week11/Main.kt
@@ -1,2 +1,12 @@
 package oop_121788_abthal.week11
 
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    // Testing basic extension functions
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    // Testing nullable receiver extension
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
--- src/oop_121788_abthal/week11/ScopeLetRun.kt
+++ src/oop_121788_abthal/week11/ScopeLetRun.kt
@@ -1,2 +1,21 @@
 package oop_121788_abthal.week11
 
+fun main() {
+    println("=== TEST LET FUNCTION ===")
+    val name: String? = "Alexander"
+
+    println("\n=== TEST RUN FUNCTION ===")
+    val result = "Kotlin".run {
+        println("Memproses kata: ${this}") // 'this' refers to "Kotlin"
+        length * 2 // Returns the result of this calculation
+    }
+    println("Hasil kalkulasi run: $result")
+
+    // Execute the block only if the object is not null
+    val length = name?.let {
+        println("Nama terdeteksi: $it") // 'it' refers to 'name'
+        it.length // Returns the value of the last expression
+    }
+
+    println("Panjang nama: $length")
+}
