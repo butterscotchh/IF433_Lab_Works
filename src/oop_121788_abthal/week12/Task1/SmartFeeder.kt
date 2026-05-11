1335ba0eea348ea5f918b7ce414c84aed4d5db71
butterscotchh <sampansedang@gmail.com>
2026-05-11 21:27:27 +0700

    week12: (task) handle feeding success using onSuccess

--- src/oop_121788_abthal/week12/Task1/SmartFeeder.kt
+++ src/oop_121788_abthal/week12/Task1/SmartFeeder.kt
@@ -37,6 +37,9 @@ fun main() {
         runCatching {
             dispenseKibble(30, currentKibbleStock, false)
         }.onSuccess { newStock ->
+            currentKibbleStock = newStock
+            println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
+        }.onFailure { error ->
         }
     }
 }
