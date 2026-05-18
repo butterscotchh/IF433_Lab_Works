d77101946be555ce049ca8dd9f19f86165a79006
butterscotchh <sampansedang@gmail.com>
2026-05-19 04:42:29 +0700

    week13: (task) test complete crypto trade logger pipeline

--- src/oop_121788_abthal/week13/Task1/CryptoTradeLogger.kt
+++ src/oop_121788_abthal/week13/Task1/CryptoTradeLogger.kt
@@ -61,4 +61,9 @@ fun main() {
 
     val loadedData = loadTrades("crypto_trades.csv")
     val totalPnl = loadedData.sumOf { it.pnl }
+
+    println("\n=== VALID TRADES SYSTEM ===")
+    loadedData.forEach { println(it) }
+
+    println("---- TOTAL PnL BERSIH: $totalPnl ----")
 }
