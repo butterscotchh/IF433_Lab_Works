639d51f2d470f47dbbc9f19553272aa31c19a83c
butterscotchh <sampansedang@gmail.com>
2026-05-26 09:23:27 +0700

    week14: inject Database abstraction to fix DIP

--- src/oop_121788_abthal/week14/DipRefactored.kt
+++ src/oop_121788_abthal/week14/DipRefactored.kt
@@ -1,2 +1,9 @@
 package oop_121788_abthal.week14
 
+interface Database {
+    fun query(sql: String): List<String>
+}
+
+class PostgresDatabase : Database {
+    override fun query(sql: String): List<String> = listOf("pg_data1", "pg_data2")
+}
--- src/oop_121788_abthal/week14/DipViolation.kt
+++ src/oop_121788_abthal/week14/DipViolation.kt
@@ -1,2 +1,10 @@
 package oop_121788_abthal.week14
 
+class MySQLDatabase {
+    fun query(sql: String): List<String> = listOf("data1", "data2")
+}
+
+class UserService {
+    private val database = MySQLDatabase() // Hard-coded! Tightly coupled!
+    fun getUser(id: Int): List<String> = database.query("SELECT * FROM users WHERE id=$id")
+}
