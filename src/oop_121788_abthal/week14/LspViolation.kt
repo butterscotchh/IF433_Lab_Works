db37a0fc5ba0e4773c20100338d9a1e097afd779
butterscotchh <sampansedang@gmail.com>
2026-05-26 09:06:23 +0700

    week14: implement Shape independently to fix LSP

--- src/oop_121788_abthal/week14/LspViolation.kt
+++ src/oop_121788_abthal/week14/LspViolation.kt
@@ -1,2 +1,14 @@
 package oop_121788_abthal.week14
 
+open class Rectangle {
+    open var width: Int = 0
+    open var height: Int = 0
+    fun area() = width * height
+}
+
+class Square : Rectangle() {
+    override var width: Int = 0
+        set(value) { field = value; super.height = value }
+    override var height: Int = 0
+        set(value) { field = value; super.width = value }
+}
